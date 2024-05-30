e4ac91050bc7c9535608733c8231c0d19bebbd05
Digital Republic <krenar@republika.tv>
2024-05-30 16:47:23 +0200

    atomic structure design and managing state of components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
-import ProjectsSidebar from "./components/ProjectsSidebar";
-import NewProject from "./components/NewProject";
+import ProjectsSidebar from "./components/Molecules/ProjectsSidebar";
+import NewProject from "./components/Molecules/NewProject";
+import NoProjectSelected from "./components/Atoms/NoProjectSelected";
+import { useState } from "react";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  let content;
+
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
