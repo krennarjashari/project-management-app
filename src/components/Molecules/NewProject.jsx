39e523a3ec45bc9a28773789b5b0897084f43fb2
Digital Republic <krenar@republika.tv>
2024-05-31 10:18:10 +0200

    validation of inputs

--- src/components/Molecules/NewProject.jsx
+++ src/components/Molecules/NewProject.jsx
@@ -1,7 +1,9 @@
 import { useRef } from "react";
 import Input from "../Atoms/Input";
+import Modal from "../Atoms/Modal";
 
-const NewProject = ({ onAdd }) => {
+const NewProject = ({ onAdd, onCancel }) => {
+  const modal = useRef();
   const title = useRef();
   const description = useRef();
   const dueDate = useRef();
@@ -11,7 +13,14 @@ const NewProject = ({ onAdd }) => {
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
-    //validation..
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
 
     onAdd({
       title: enteredTitle,
@@ -21,28 +30,39 @@ const NewProject = ({ onAdd }) => {
   }
 
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" label="Title" ref={title} />
-        <Input label="Decription" textarea ref={description} />
-        <Input type="date" label="Due Date" ref={dueDate} />
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className="text-xl font-bold text-stone-700 my-4">Invalid Input</h2>
+        <p className="text-stone-600 mb-4">
+          Please make sure you provide a valid value for every input field.
+        </p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button
+              className="text-stone-800 hover:text-stone-950"
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" label="Title" ref={title} />
+          <Input label="Decription" textarea ref={description} />
+          <Input type="date" label="Due Date" ref={dueDate} />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
